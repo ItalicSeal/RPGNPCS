bf2f7f25e4b68be1a21f3fa657d0480ce0fa36bf
ItalicSeal <harry420au@gmail.com>
2023-09-26 20:38:01 +1000

    Added Source And updated pom.xml

---
+++ src/main/java/me/italicseal/rpgnpcs/RPGNPCS.java
@@ -0,0 +1,17 @@
+package me.italicseal.rpgnpcs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGNPCS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
